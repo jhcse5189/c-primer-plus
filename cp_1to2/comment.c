975a3801188e61f98dcdec6840f93176a0466eef
jhcse5189 <jhcse5189@gmail.com>
2019-08-19 13:35:04 +0900

    PE2_7

---
+++ cp_1to2/comment.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+	/* is it works?
+	int x = 100;
+	printf("wow.. it works still!\n");
+	/* may..be? */
+	return 0;
+}
---
+++ cp_1to2/getchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void)
+{
+	int cats;
+	
+	printf("How many cats do you have?\n");
+	scanf("%d", &cats);
+	printf("So you have %d cat(s)!\n", cats);
+	
+	getchar();
+	getchar();
+	return 0;
+}
